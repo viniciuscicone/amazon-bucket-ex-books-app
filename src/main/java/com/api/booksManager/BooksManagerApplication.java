357b2eecd8c3ac0b41780b7cd772f2a51373e9d4
Vinicius Barbosa <viniciuscicone@gmail.com>
2024-07-19 22:22:48 -0300

    iniciate

---
+++ src/main/java/com/api/booksManager/BooksManagerApplication.java
@@ -0,0 +1,13 @@
+package com.api.booksManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooksManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BooksManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/booksManager/BooksManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.booksManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooksManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

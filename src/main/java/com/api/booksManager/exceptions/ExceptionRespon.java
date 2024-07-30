50e2f2389a710972f6163f46a8487f15f5ce9fe7
Vinicius Barbosa <viniciuscicone@gmail.com>
2024-07-30 20:20:12 -0300

    Adding exception response

--- src/main/java/com/api/booksManager/exceptions/ExceptionRespon.java
+++ src/main/java/com/api/booksManager/exceptions/ExceptionRespon.java
@@ -6,5 +6,29 @@ import java.util.Collections;
 import java.util.List;
 
 public class ExceptionRespon {
+    private String error;
+    private Integer status;
+    private List<String> messages;
+    private Instant timestamp;
 
+    public ExceptionRespon(List<String> messages, String error, Integer status) {
+        setMessages(messages);
+        this.error = error;
+        this.status = status;
+        this.timestamp = Instant.now();
+    }
+
+    public List<String> getMessages() {
+
+        return messages == null ? null : new ArrayList<>(messages);
+    }
+
+    public final void setMessages(List<String> messages) {
+
+        if (messages == null) {
+            this.messages = null;
+        } else {
+            this.messages = Collections.unmodifiableList(messages);
+        }
+    }
 }

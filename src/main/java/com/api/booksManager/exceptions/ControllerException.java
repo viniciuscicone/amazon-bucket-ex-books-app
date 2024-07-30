27534699eb6d4c9d1d1a8f5a27bca91f9355cb50
Vinicius Barbosa <viniciuscicone@gmail.com>
2024-07-30 20:12:09 -0300

    updating exception

--- src/main/java/com/api/booksManager/exceptions/ControllerException.java
+++ src/main/java/com/api/booksManager/exceptions/ControllerException.java
@@ -1,5 +1,26 @@
 package com.api.booksManager.exceptions;
 
-public class ControllerException {
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+
+@ControllerAdvice
+class ControllerException extends RuntimeException {
+
+    @ResponseBody
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity handleException(MethodArgumentNotValidException e) {
+
+        MessageReturn generic = new MessageReturn(
+                HttpStatus.BAD_REQUEST.getReasonPhrase(),
+                e.getFieldError().getDefaultMessage(),
+                "Um problema com este campo: " + e.getFieldError().getField());
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(generic);
+    }
 
 }

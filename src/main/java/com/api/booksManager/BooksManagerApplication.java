b3950ff8a5edaf4dedab42c95c918c4f06ebe7b8
Vinicius Barbosa <viniciuscicone@gmail.com>
2024-08-01 20:45:01 -0300

    dockerfile test container enviroment

--- src/main/java/com/api/booksManager/BooksManagerApplication.java
+++ src/main/java/com/api/booksManager/BooksManagerApplication.java
@@ -2,8 +2,10 @@ package com.api.booksManager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+
+@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class })
 public class BooksManagerApplication {
 
 	public static void main(String[] args) {

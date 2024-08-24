9aabc1984a1bdd9704759adfd15cbf5ce3835709
Vinicius Barbosa <viniciuscicone@gmail.com>
2024-08-24 19:29:40 -0300

    updating project

---
+++ src/main/java/com/api/booksManager/controller/BooksControlers.java
@@ -0,0 +1,4 @@
+package com.api.booksManager.controller;
+
+public class BooksControlers {
+}
--- src/main/java/com/api/booksManager/domain/Book.java
+++ src/main/java/com/api/booksManager/domain/Book.java
@@ -1,4 +1,18 @@
 package com.api.booksManager.domain;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+import java.util.UUID;
+
+@Table(name = "Book")
+@Entity
 public class Book {
+
+    @Id
+    @GeneratedValue
+    UUID id;
+    String name;
 }
